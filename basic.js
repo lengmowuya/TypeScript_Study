431c4d451502cd296f872cd39a54a4442ad8079b
lengmowuya <3537168410@qq.com>
2022-06-30 22:57:22 +0800

    TypeScript+1

--- basic.js
+++ basic.js
@@ -1,1 +1,25 @@
 console.log("Hello,TS!");
+// 以前
+var normal = 10;
+normal = 123;
+// TypeScript
+var a = 10;
+a = 123456;
+var b = "10";
+b = "Hello,B!";
+var c = false;
+c = true;
+// 此行报错，a的类型是number，不能赋值string
+// 不过即使报错，也会编译通过
+// a="ts"; 
+// 默认编译成var的ES3
+function sum(a, b) {
+    return a + b;
+}
+sum(123, 456);
+// 字符串类型报错
+// sum(123,"456");
+// 参数传多了报错
+// sum(123,456,899);
+// 参数传少了报错
+sum(123);
--- basic.ts
+++ basic.ts
@@ -1,1 +1,37 @@
 console.log("Hello,TS!");
+
+// 以前
+let normal = 10;
+normal = 123;
+// TypeScript
+let a: number = 10;
+a = 123456;
+let b: string = "10";
+b = "Hello,B!";
+let c :boolean = false;
+c = true;
+
+// 此行报错，a的类型是number，不能赋值string
+// 不过即使报错，也会编译通过
+// a="ts"; 
+
+// 默认编译成var的ES3
+
+function sum(a:number,b:number){
+    return a + b;
+}
+
+sum(123,456);
+// 字符串类型报错
+// sum(123,"456");
+// 参数传多了报错
+// sum(123,456,899);
+// 参数传少了报错
+// sum(123);
+
+function add(a:number,b:number):number{
+    return a +b;
+}
+let result =  sum(123,456);
+// let error = "hi";
+// error = sum(1,4);
