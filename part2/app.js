a71b83332229280704df27e026e731c45e257234
lengmowuya <3537168410@qq.com>
2022-07-18 23:04:48 +0800

    update

---
+++ part2/app.js
@@ -0,0 +1,5 @@
+console.log('hello');
+var a = 10;
+var b = 33;
+// tsc app.ts -w 会监视文件变化
+var c = "hello";
---
+++ part2/app.ts
@@ -0,0 +1,7 @@
+console.log('hello');
+let a = 10;
+let b = 33;
+
+
+// tsc app.ts -w 会监视文件变化
+let c = "hello";
