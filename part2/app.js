108081206bbed8343b7930e408d9a670d2f8d068
lengmowuya <3537168410@qq.com>
2022-07-19 22:43:47 +0800

    配置编译文件

--- part2/app.js
+++ part2/app.js
@@ -3,3 +3,5 @@ var a = 10;
 var b = 33;
 // tsc app.ts -w 会监视文件变化
 var c = "hello";
+// tsc编译所有.ts后缀的文件
+// 需要编译配置文件tsconfig.json
--- part2/app.ts
+++ part2/app.ts
@@ -5,3 +5,6 @@ let b = 33;
 
 // tsc app.ts -w 会监视文件变化
 let c = "hello";
+
+// tsc编译所有.ts后缀的文件
+// 需要编译配置文件tsconfig.json
